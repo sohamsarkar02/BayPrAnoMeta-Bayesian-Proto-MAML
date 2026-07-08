329c7c43dbfb82af12f6638ce107efe8f1b7bdb8
sohamsarkar02 <sarkarzico2002@gmail.com>
2026-07-08 23:21:51 +0530

    Update train_patchcore_leave1out_50pct.py

--- PatchCore/train_patchcore_leave1out_50pct.py
+++ PatchCore/train_patchcore_leave1out_50pct.py
@@ -21,8 +21,7 @@ from utils.utils_data_fedcontrast_leave1out import (
 # ---------------- CONFIG ---------------- #
 
 BASE_ROOT = (
-    "/Users/sohamsarkar/Desktop/Projects/"
-    "Research Projects/MAML/ML Project/Datasets/"
+    "/.../"
     "MVTech AD Dataset"
 )
 
@@ -34,12 +33,12 @@ CLIENTS = [
 
 DEVICE = torch.device("cpu")
 
-# 🔹 NEW output directory (separate from all others)
+
 OUT_DIR = "runs/patchcore_shared_encoder_50pct"
 MEMORY_DIR = f"{OUT_DIR}/memory_bank"
 os.makedirs(MEMORY_DIR, exist_ok=True)
 
-CORESET_FRACTION = 0.50   # 🔹 50% coreset
+CORESET_FRACTION = 0.50   
 
 random.seed(42)
 np.random.seed(42)
